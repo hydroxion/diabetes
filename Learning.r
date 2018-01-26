f75faa3dc96dfb4d29b9ce56637c7a04a0c0866c
Walter <Sphinxs@users.noreply.github.com>
2018-01-26 02:15:52 -0200

    Diabetes

--- Learning.r
+++ Learning.r
@@ -5,6 +5,8 @@ library(clValid)
 
 library(dbscan)
 
+library(fpc)
+
 
 data <- read.csv(file='Dataset.csv', sep=",", header=TRUE)
 
@@ -15,9 +17,13 @@ dnk <- dunn(dist(data), km$cluster)
 
 cat("\nDunn, K-means \033[31m:\033[37m ", dnk, "\n\n")
 
+plotcluster(data, km$cluster)
+
 
 db <- dbscan(data, 60, minPts=6)
 
 dnb <- dunn(dist(data), db$cluster)
 
 cat("\nDunn, Db-scan \033[31m:\033[37m ", dnb, "\n\n")
+
+plot(data, col=db$cluster)
